6a46b6961cd0bb12982f1562166f18deb0222dfb
Derrick Carr <whyderrick@gmail.com>
2021-11-30 09:38:48 -0800

    Remove evil-snipe so the s key works to insert again

--- doom.d/config.el
+++ doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Derrick Carr"
+      user-mail-address "whyderrick@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -52,3 +52,7 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; I don't want evil-snipe-mode, but I do want its incremental highlighting of
+;; of matches within a link
+(remove-hook 'doom-first-input-hook #'evil-snipe-mode)

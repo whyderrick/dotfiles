bb1bef93c05a104df7a9c644b90ac94b11f97295
Derrick Carr <whyderrick@gmail.com>
2022-07-10 21:18:17 -0700

    Add typescript support to my emacs environment

--- doom.d/config.el
+++ doom.d/config.el
@@ -56,3 +56,35 @@
 ;; I don't want evil-snipe-mode, but I do want its incremental highlighting of
 ;; of matches within a link
 (remove-hook 'doom-first-input-hook #'evil-snipe-mode)
+
+;; Configure Tide (Typescript IDE)
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; Enable Tide for tsx files
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+           (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(after! (:and flycheck tide)
+        (flycheck-add-mode 'typescript-tslint 'web-mode))
